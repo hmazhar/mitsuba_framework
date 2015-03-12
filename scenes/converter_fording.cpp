7cee4854deb5dd4405ed381fce2fbe67b0e14187
Hammad Mazhar <hammad.maz@gmail.com>
2015-03-12 17:06:28 -0500

    Update fording converter to do partio output

--- scenes/converter_fording.cpp
+++ scenes/converter_fording.cpp
@@ -1,6 +1,6 @@
 #include "converter_general.h"
 #include "MitsubaGenerator.h"
-
+#include "Partio.h"
 using namespace std;
 using namespace chrono;
 
@@ -46,7 +46,10 @@ int main(int argc, char* argv[]) {
 
   data_document.AddShape("chassis", Vector(1, 1, 1), pos + offset, rot);
   Vector camera_pos = pos + offset;
-  camera_pos.y = 10;
+  camera_pos.y = 6;
+  // mo
+  camera_pos.z += 1;
+  camera_pos.x += 3;
   data_document.AddSensor(camera_pos, pos + offset, Vector(0, 0, 1));
 
   SkipLine(data_stream, 256);
@@ -69,6 +72,34 @@ int main(int argc, char* argv[]) {
   SkipLine(data_stream, 525);
   ProcessPovrayLine(data_stream, pos, vel, scale, rot);
   data_document.AddShape("wheel", Vector(1, 1, 1), pos, rot);
+  SkipLine(data_stream, 7);
+
+  data_document.AddShape("fluid", ChVector<>(1), ChVector<>(0), ChQuaternion<>(1, 0, 0, 0));
+  //
+  Partio::ParticlesDataMutable& data_p = *Partio::create();
+  Partio::ParticleAttribute positionAttr = data_p.addAttribute("position", Partio::VECTOR, 3);
+  Partio::ParticleAttribute velocityAttr = data_p.addAttribute("v", Partio::VECTOR, 3);
+
+  while (data_stream.fail() == false) {
+    ProcessPovrayLine(data_stream, pos, vel, scale, rot);
+    if (data_stream.fail() == false) {
+      Partio::ParticleIndex index = data_p.addParticle();
+      float* pos_partio = data_p.dataWrite<float>(positionAttr, index);
+      pos_partio[0] = pos.x;
+      pos_partio[1] = pos.y;
+      pos_partio[2] = pos.z;
+      float* vel_partio = data_p.dataWrite<float>(velocityAttr, index);
+      vel_partio[0] = vel.x;
+      vel_partio[1] = vel.y;
+      vel_partio[2] = vel.z;
+      //      //std::cout<<vel.Length()<<std::endl;
+      // data_document.AddShape("sphere", ChVector<>(0.03), pos, rot);
+    }
+  }
+  //
+  stringstream partio_ss;
+  partio_ss << argv[1] << ".bgeo";
+  Partio::write(partio_ss.str().c_str(), data_p);
 
   //  while (data_stream.fail() == false) {
   //    ProcessPovrayLine(data_stream, pos, vel, scale, rot);
