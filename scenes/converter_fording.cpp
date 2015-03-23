268300c6e77b72e0a7995de38d73c08305773928
Hammad Mazhar <hammad.maz@gmail.com>
2015-03-23 16:57:14 -0500

    Render with spheres

--- scenes/converter_fording.cpp
+++ scenes/converter_fording.cpp
@@ -74,39 +74,39 @@ int main(int argc, char* argv[]) {
   data_document.AddShape("wheel", Vector(1, 1, 1), pos, rot);
   SkipLine(data_stream, 7);
 
-  data_document.AddShape("fluid", ChVector<>(1), ChVector<>(0), ChQuaternion<>(1, 0, 0, 0));
+  //  data_document.AddShape("fluid", ChVector<>(1), ChVector<>(0), ChQuaternion<>(1, 0, 0, 0));
+  //  //
+  //  Partio::ParticlesDataMutable& data_p = *Partio::create();
+  //  Partio::ParticleAttribute positionAttr = data_p.addAttribute("position", Partio::VECTOR, 3);
+  //  Partio::ParticleAttribute velocityAttr = data_p.addAttribute("v", Partio::VECTOR, 3);
   //
-  Partio::ParticlesDataMutable& data_p = *Partio::create();
-  Partio::ParticleAttribute positionAttr = data_p.addAttribute("position", Partio::VECTOR, 3);
-  Partio::ParticleAttribute velocityAttr = data_p.addAttribute("v", Partio::VECTOR, 3);
+  //  while (data_stream.fail() == false) {
+  //    ProcessPovrayLine(data_stream, pos, vel, scale, rot);
+  //    if (data_stream.fail() == false) {
+  //      Partio::ParticleIndex index = data_p.addParticle();
+  //      float* pos_partio = data_p.dataWrite<float>(positionAttr, index);
+  //      pos_partio[0] = pos.x;
+  //      pos_partio[1] = pos.y;
+  //      pos_partio[2] = pos.z;
+  //      float* vel_partio = data_p.dataWrite<float>(velocityAttr, index);
+  //      vel_partio[0] = vel.x;
+  //      vel_partio[1] = vel.y;
+  //      vel_partio[2] = vel.z;
+  //      //      //std::cout<<vel.Length()<<std::endl;
+  //      // data_document.AddShape("sphere", ChVector<>(0.03), pos, rot);
+  //    }
+  //  }
+  //  //
+  //  stringstream partio_ss;
+  //  partio_ss << argv[1] << ".bgeo";
+  //  Partio::write(partio_ss.str().c_str(), data_p);
 
   while (data_stream.fail() == false) {
     ProcessPovrayLine(data_stream, pos, vel, scale, rot);
     if (data_stream.fail() == false) {
-      Partio::ParticleIndex index = data_p.addParticle();
-      float* pos_partio = data_p.dataWrite<float>(positionAttr, index);
-      pos_partio[0] = pos.x;
-      pos_partio[1] = pos.y;
-      pos_partio[2] = pos.z;
-      float* vel_partio = data_p.dataWrite<float>(velocityAttr, index);
-      vel_partio[0] = vel.x;
-      vel_partio[1] = vel.y;
-      vel_partio[2] = vel.z;
-      //      //std::cout<<vel.Length()<<std::endl;
-      // data_document.AddShape("sphere", ChVector<>(0.03), pos, rot);
+      data_document.AddShape("sphere", ChVector<>(0.03), pos, rot);
     }
   }
-  //
-  stringstream partio_ss;
-  partio_ss << argv[1] << ".bgeo";
-  Partio::write(partio_ss.str().c_str(), data_p);
-
-  //  while (data_stream.fail() == false) {
-  //    ProcessPovrayLine(data_stream, pos, vel, scale, rot);
-  //    if (data_stream.fail() == false) {
-  //      data_document.AddShape("sphere", ChVector<>(0.011), pos, rot);
-  //    }
-  //  }
   stringstream output_file_ss;
   output_file_ss << argv[1] << ".xml";
   data_document.Write(output_file_ss.str());
