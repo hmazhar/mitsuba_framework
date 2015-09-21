6c1224c9a395681d428198d95e3f4886b9a8cc44
Hammad Mazhar <hammad.maz@gmail.com>
2015-09-21 12:04:13 -0500

    Add function to read in just position and velocity

--- converter_general.h
+++ converter_general.h
@@ -7,75 +7,83 @@
 #include <collision/ChCModelBullet.h>
 
 void ReadCompressed(std::string filename, std::string& data) {
-  gzFile gz_file = gzopen(filename.c_str(), "rb");
-  unsigned long int size;
-  gzread(gz_file, (void*)&size, sizeof(size));
-  data.resize(size / sizeof(char));
+    gzFile gz_file = gzopen(filename.c_str(), "rb");
+    unsigned long int size;
+    gzread(gz_file, (void*)&size, sizeof(size));
+    data.resize(size / sizeof(char));
 
-  gzread(gz_file, (void*)data.data(), size);
-  gzclose(gz_file);
+    gzread(gz_file, (void*)data.data(), size);
+    gzclose(gz_file);
 }
 
 void SkipLine(std::stringstream& ifile, int number = 1) {
-  std::string temp;
-  for (int i = 0; i < number; i++) {
-    getline(ifile, temp);
-  }
+    std::string temp;
+    for (int i = 0; i < number; i++) {
+        getline(ifile, temp);
+    }
 }
 void ProcessLine(std::stringstream& ifile,
                  chrono::ChVector<>& pos,
                  chrono::ChVector<>& vel,
                  chrono::ChQuaternion<>& quat) {
-  std::string temp;
-  std::getline(ifile, temp);
-  std::stringstream ss(temp);
-  double junk;
-  ss >> pos.x >> pos.y >> pos.z;
-  ss >> quat.e0 >> quat.e1 >> quat.e2 >> quat.e3;
-  ss >> vel.x >> vel.y >> vel.z;
-  ss >> junk >> junk >> junk;
+    std::string temp;
+    std::getline(ifile, temp);
+    std::stringstream ss(temp);
+    double junk;
+    ss >> pos.x >> pos.y >> pos.z;
+    ss >> quat.e0 >> quat.e1 >> quat.e2 >> quat.e3;
+    ss >> vel.x >> vel.y >> vel.z;
+    ss >> junk >> junk >> junk;
 }
 int ProcessPovrayLine(std::stringstream& ifile,
                       chrono::ChVector<>& pos,
                       chrono::ChVector<>& vel,
                       chrono::ChVector<>& rad,
                       chrono::ChQuaternion<>& quat) {
-  std::string temp;
-  std::getline(ifile, temp);
-  std::stringstream ss(temp);
-  int type;
-  rad = chrono::ChVector<>(1, 1, 1);
-  ss >> pos.x >> pos.y >> pos.z;
-  ss >> quat.e0 >> quat.e1 >> quat.e2 >> quat.e3;
-  ss >> vel.x >> vel.y >> vel.z;
-  ss >> type;
-  switch (type) {
-    case chrono::collision::SPHERE:
-      ss >> rad.x;
-      rad.y = rad.z = rad.x;
-      break;
-    case chrono::collision::ELLIPSOID:
-      ss >> rad.x >> rad.y >> rad.z;
-      break;
-    case chrono::collision::BOX:
-      ss >> rad.x >> rad.y >> rad.z;
-      break;
-    case chrono::collision::CYLINDER:
-      ss >> rad.x >> rad.y;
-      rad.z = rad.x;
-      break;
-    case chrono::collision::CONE:
-      ss >> rad.x >> rad.y;
-      rad.z = rad.x;
-      break;
-    case chrono::collision::CAPSULE:
-      ss >> rad.x >> rad.y;
-      rad.z = rad.x;
-      break;
-    default:
-      // type is -1 (triangle mesh)
-      return -1;
-      break;
-  }
-  return type;
+    std::string temp;
+    std::getline(ifile, temp);
+    std::stringstream ss(temp);
+    int type;
+    rad = chrono::ChVector<>(1, 1, 1);
+    ss >> pos.x >> pos.y >> pos.z;
+    ss >> quat.e0 >> quat.e1 >> quat.e2 >> quat.e3;
+    ss >> vel.x >> vel.y >> vel.z;
+    ss >> type;
+    switch (type) {
+        case chrono::collision::SPHERE:
+            ss >> rad.x;
+            rad.y = rad.z = rad.x;
+            break;
+        case chrono::collision::ELLIPSOID:
+            ss >> rad.x >> rad.y >> rad.z;
+            break;
+        case chrono::collision::BOX:
+            ss >> rad.x >> rad.y >> rad.z;
+            break;
+        case chrono::collision::CYLINDER:
+            ss >> rad.x >> rad.y;
+            rad.z = rad.x;
+            break;
+        case chrono::collision::CONE:
+            ss >> rad.x >> rad.y;
+            rad.z = rad.x;
+            break;
+        case chrono::collision::CAPSULE:
+            ss >> rad.x >> rad.y;
+            rad.z = rad.x;
+            break;
+        default:
+            // type is -1 (triangle mesh)
+            return -1;
+            break;
+    }
+    return type;
+}
+
+void ProcessPosVel(std::stringstream& ifile, chrono::ChVector<>& pos, chrono::ChVector<>& vel) {
+    std::string temp;
+    std::getline(ifile, temp);
+    std::stringstream ss(temp);
+    ss >> pos.x >> pos.y >> pos.z;
+    ss >> vel.x >> vel.y >> vel.z;
 }
