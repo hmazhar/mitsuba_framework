242d1a8a76406ca319317e19fe074a90cc8726c3
Hammad Mazhar <hammad.maz@gmail.com>
2015-10-23 13:00:09 -0500

    Update drive converter to read binary PBD data

--- scenes/converter_drive.cpp
+++ scenes/converter_drive.cpp
@@ -22,10 +22,14 @@ int main(int argc, char *argv[]) {
   stringstream input_file_ss, input_file_vehicle;
   input_file_ss << "data_" << argv[1] << ".dat";
   input_file_vehicle << "vehicle_" << argv[1] << ".dat";
+  std::ifstream ifile;
+  OpenBinary(input_file_ss.str(), ifile);
 
-  string data;
-  ReadCompressed(input_file_ss.str(), data);
-  std::replace(data.begin(), data.end(), ',', '\t');
+  std::vector<half3> position;
+  std::vector<half3> velocity;
+
+  ReadBinary(ifile, position);
+  CloseBinary(ifile);
 
   string data_v;
   ReadCompressed(input_file_vehicle.str(), data_v);
@@ -38,25 +42,24 @@ int main(int argc, char *argv[]) {
   }
   MitsubaGenerator data_document(output_file_ss.str());
 
-  stringstream data_stream(data);
   stringstream vehicle_stream(data_v);
 
   ChVector<> pos, vel, scale;
   ChQuaternion<> rot;
   int count = 0;
-  while (data_stream.fail() == false) {
-    ProcessPosVel(data_stream, pos, vel);
 
-    double v = vel.Length();
+  for (int i = 0; i < position.size(); i++) {
+    pos.x = position[i].x;
+    pos.y = position[i].y;
+    pos.z = position[i].z;
 
-    if (data_stream.fail() == false) {
-      data_document.AddCompleteShape("sphere", "diffuse", VelToColor(v), .015,
-                                     pos, QUNIT);
-    }
+    // double v = vel.Length() * 2;
 
-    if (count % 1000 == 0) {
-      std::cout << count << std::endl;
-    }
+    data_document.AddShape("sphere", .1, pos, QUNIT);
+    //      data_document.AddCompleteShape("sphere", "diffuse", VelToColor(v),
+    //      .015,
+    //                                     pos, QUNIT);
+
     count++;
   }
   // std::cout << data_v << std::endl;
@@ -67,6 +70,13 @@ int main(int argc, char *argv[]) {
   rot = rot * q;
   Vector offset = rot.Rotate(Vector(-0.055765, 0, -0.52349));
   data_document.AddShape("chassis", Vector(1, 1, 1), pos + offset, rot);
+
+  Vector camera_pos = pos + offset;
+  camera_pos.z = 4;
+  camera_pos.y -= 8;
+  camera_pos.x += 0;
+  data_document.AddSensor(camera_pos, pos + offset, Vector(0, 0, 1));
+
   ProcessPovrayLine(vehicle_stream, pos, vel, scale, rot);
   data_document.AddShape("wheel_R", Vector(1, 1, 1), pos, rot);
   ProcessPovrayLine(vehicle_stream, pos, vel, scale, rot);
